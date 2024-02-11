7299389a36833315c658219265f0ed2b3db491b3
lucas-santana-risk3 <lucaslucal75@gmail.com>
2024-02-11 11:36:53 -0300

    Criacao primeiro dag

--- dags/meu_primeiro_dag.py
+++ dags/meu_primeiro_dag.py
@@ -1,5 +1,8 @@
-from airflow.import DAG
-from air
+from airflow.models import DAG
+from airfrow.utils.dates import days_ago
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash_operator import BashOperator
+
 
 #Directed Acyclic Graphs
 with DAG(
@@ -7,3 +10,14 @@ with DAG(
     start_date=days_ago(1),
     schedule_interval='@daily'
 ) as dag:
+    
+    tarefa_1 = EmptyOperator(task_id = 'tarefa_1')
+    tarefa_2 = EmptyOperator(task_id = 'tarefa_2')
+    tarefa_3 = EmptyOperator(task_id = 'tarefa_3')
+    tarefa_4 = BashOperator(
+        task_id = 'cria_pasta',
+        bash_command = 'mkdir -p "/home/lucas/Documents/airflowalura/pasta"'
+        )
+    
+    tarefa_1 >> [tarefa_2, tarefa_3]
+    tarefa_3 >> tarefa_4
